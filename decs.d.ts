7ec56a9ca070742dab646f4f6f5136865320c047
Luis G. Villegas <luisguveal@gmail.com>
2022-02-11 22:09:27 -0430

    Declare non-ts libraries

---
+++ decs.d.ts
@@ -0,0 +1,7 @@
+declare module "@strapi/design-system/Tabs";
+declare module "@strapi/design-system/Tab";
+declare module "@strapi/design-system/TabGroup";
+declare module "@strapi/design-system/TabPanels";
+declare module "@strapi/design-system/TabPanel";
+declare module "@strapi/design-system/ThemeProvider";
+declare module "@strapi/design-system/themes";
