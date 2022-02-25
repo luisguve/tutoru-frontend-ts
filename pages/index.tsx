bd13317cdbced66e60ed5b2e44107af784fcc593
Luis G. Villegas <luisguveal@gmail.com>
2022-02-25 10:59:21 -0430

    Updated page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,33 @@
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
-import { getCoursesSummary, ICourseSummary } from "../lib/content"
+import { getCategoriesSummary, ICategorySummary } from "../lib/content"
 import { loadNavigation, INavigationItem } from "../lib/metadata"
-import CourseSummary from "../components/Category/CourseSummary"
+import { CategorySummary } from "../components/Category/CategoryIndex"
 
 export async function getStaticProps() {
-  const { courses } = await getCoursesSummary()
+  const { categories } = await getCategoriesSummary()
   const navigation = await loadNavigation()
   return {
     props: {
-      courses,
+      categories,
       navigation
     }
   }
 }
 
 interface HomeProps {
-  courses: ICourseSummary[];
+  categories: ICategorySummary[];
   navigation: INavigationItem[];
 }
 export default function Home(props: HomeProps) {
-  const courses = props.courses.map(c => <CourseSummary data={c} key={c.slug} />)
+  const categories = props.categories.map(c => {
+    return (
+      <div className="mb-5" key={c.slug}>
+        <CategorySummary data={c} displayLink />
+      </div>
+    )
+  })
   return (
     <Layout
       title="abc"
@@ -31,8 +37,8 @@ export default function Home(props: HomeProps) {
       isHome
     >
       <section className="p-1">
-        <h1>LATEST COURSES</h1>
-        {courses}
+        <h1 className="mb-4 text-center">Categories</h1>
+        {categories}
       </section>
     </Layout>
   )
