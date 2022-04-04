ce41ede9bc6ac46414eb3d3abc44ab3e2cc3b1f6
Luis G. Villegas <luisguveal@gmail.com>
2022-04-04 14:55:08 -0430

    Modified Playlist component to be used as PlaylistSummary in CoursePage and updated counter of lectures in each module

--- components/Category/LecturesList/Playlist.tsx
+++ components/Category/LecturesList/Playlist.tsx
@@ -9,9 +9,10 @@ import styles from "../../../styles/PaginaCurso.module.scss"
 
 interface PlaylistProps {
   modules: IModule[];
-  changeLecture: (_?: number) => Promise<void>;
-  currentLectureID: number | null;
   courseID: number;
+  playable?: boolean;
+  changeLecture?: (_?: number) => Promise<void>;
+  currentLectureID?: number | null;
 }
 const Playlist = (props: PlaylistProps) => {
   const {
@@ -19,30 +20,58 @@ const Playlist = (props: PlaylistProps) => {
     changeLecture,
     currentLectureID,
     courseID,
+    playable
   } = props
 
   return (
     <div className="accordion" id="playlist-summary-accordion">
       {
-        modules.map((module, idx) => (
-          <div className="accordion-item" key={`module-${module.id}`}>
-            <h2 className="accordion-header" id={`playlist-accordion-header-${idx}`}>
-              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#playlist-accordion-body-${idx}`} aria-expanded="false" aria-controls={`playlist-accordion-body-${idx}`}>
-                {module.title || `${idx+1}. Untitled module`} - {formatDuration(module.duration)}
-              </button>
-            </h2>
-            <div id={`playlist-accordion-body-${idx}`} className="accordion-collapse collapse" aria-labelledby={`playlist-accordion-header-${idx}`}>
-              <div className="accordion-body p-0">
-                <ModuleLectures
-                  lectures={module.lectures}
-                  changeLecture={changeLecture}
-                  currentLectureID={currentLectureID}
-                  courseID={courseID}
-                />
+        modules.map((module, idx) => {
+
+          let isCurrentModule: boolean = false
+
+          if (currentLectureID !== null) {
+            isCurrentModule = module.lectures.some(l => l.id === currentLectureID)
+          }
+
+          let startCountAt: number = 0
+          for (let i = 0; i < idx; i++) {
+            startCountAt += module.lectures.length
+          }
+
+          return (
+            <div className="accordion-item" key={`module-${module.id}`}>
+              <h2 className="accordion-header" id={`playlist-accordion-header-${idx}`}>
+                <button
+                  className={"accordion-button" + (!isCurrentModule ? " collapsed" : "")}
+                  aria-expanded={!isCurrentModule ? "false" : "true"}
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#playlist-accordion-body-${idx}`}
+                  aria-controls={`playlist-accordion-body-${idx}`}
+                >
+                  {module.title || `${idx+1}. Untitled module`} - {formatDuration(module.duration)}
+                </button>
+              </h2>
+              <div
+                id={`playlist-accordion-body-${idx}`}
+                className={"accordion-collapse collapse" + (isCurrentModule ? " show" : "")}
+                aria-labelledby={`playlist-accordion-header-${idx}`}
+              >
+                <div className="accordion-body p-0">
+                  <ModuleLectures
+                    playable={playable}
+                    startCountAt={startCountAt}
+                    lectures={module.lectures}
+                    changeLecture={changeLecture}
+                    currentLectureID={currentLectureID}
+                    courseID={courseID}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          )
+        })
       }
     </div>
   )
@@ -50,9 +79,11 @@ const Playlist = (props: PlaylistProps) => {
 
 interface ModuleLecturesProps {
   lectures: Ilecture[];
-  changeLecture: (_?: number) => Promise<void>;
-  currentLectureID: number | null;
   courseID: number;
+  playable?: boolean;
+  changeLecture?: (_?: number) => Promise<void>;
+  currentLectureID?: number | null;
+  startCountAt: number;
 }
 const ModuleLectures = (props: ModuleLecturesProps) => {
   const {
@@ -60,6 +91,8 @@ const ModuleLectures = (props: ModuleLecturesProps) => {
     changeLecture,
     currentLectureID,
     courseID,
+    playable,
+    startCountAt
   } = props
 
   const { classesCompleted, toggleClassCompleted } = useContext(PlaylistContext)
@@ -72,12 +105,12 @@ const ModuleLectures = (props: ModuleLecturesProps) => {
         lectures.map((lecture, idx) => {
           const completed = classesCompleted.some(l => l.id === lecture.id)
           const isCurrent = currentLectureID === lecture.id
-          let classCurrent = styles["no-video-actual"]
+          let classCurrent = playable ? styles["no-video-actual"] : ""
           if (isCurrent) {
             classCurrent = "bg-dark text-light"
           }
           const handleClick = () => {
-            if (!user) {
+            if (!user || !changeLecture) {
               return
             }
             if (isCurrent) {
@@ -111,7 +144,7 @@ const ModuleLectures = (props: ModuleLecturesProps) => {
             <li
               className={`${idx!==0?"border-top":""} px-2 d-flex align-items-center ${classCurrent}`}
               key={lecture.id}
-              onClick={handleClick}
+              onClick={playable ? handleClick : undefined}
             >
               <label className={styles["checkmark-container"]} onClick={e => e.stopPropagation()}>
                 <input
@@ -124,7 +157,7 @@ const ModuleLectures = (props: ModuleLecturesProps) => {
                   className={styles["checkmark"]+(completed?` ${styles["checked"]}` : "")}
                 ></span>
               </label>
-              <span className="me-1 me-sm-2">{idx + 1}.</span>
+              <span className="me-1 me-sm-2">{startCountAt + idx + 1}.</span>
               <div className="pt-3 d-flex flex-column align-items-end flex-grow-1">
                 <p className="mb-0 small" style={{wordBreak: "break-all"}}>{lecture.title}</p>
                 <p className="small fst-italic fw-light">{formatDuration(lecture.video.duration)}</p>
